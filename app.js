3fc82f483a6fdd0c29ff4ac8dd83dc914f666699
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-09-22 17:28:46 +0200

    add initial swagger

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 require("dotenv").config();
 
 const usersRouter = require("./routes/users");
@@ -17,6 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/users", usersRouter);
 app.use("/auth", authRouter);
 app.use("/api/drinks", drinksRouter);
